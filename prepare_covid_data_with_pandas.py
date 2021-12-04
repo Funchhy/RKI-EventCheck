9bf039790f7bbf0bf23ad2f24c7db45a4cb4d269
Funchhy <71510696+Funchhy@users.noreply.github.com>
2021-12-04 11:26:47 +0100

    Landkreismatching

--- prepare_covid_data_with_pandas.py
+++ prepare_covid_data_with_pandas.py
@@ -21,8 +21,21 @@ print(resultjson['features'])
 #RKI History analyzer
 import csv
 import pandas as pd
+fields = ["Landkreis", "IdLandkreis"]
+# csv too large with unrestricted nrows
+#df = pd.read_csv('RKI_COVID19_history.csv', index_col=8, nrows=1000)
 
-df = pd.read_csv('RKI_COVID19_history.csv', index_col=8, nrows=1000)
+# read Csv in Chunks
+tp = pd.read_csv('RKI_COVID19_history.csv', iterator=True, chunksize=1000, usecols=fields)
 
-print(df.head(10))
+df = pd.concat(tp)
 
+print(df.iloc[1000000:1000005])
+
+# Find matching from Landkreis to IdLandkreis for every Landkreis
+#df[['Landkreis', 'IdLandkreis']]
+df = df.drop_duplicates(subset='Landkreis', keep='first')
+
+print(df)
+df.to_csv('LandkreisMatching.csv', encoding='utf-8')
+#df.to_csv('testOut.csv', encoding='utf-8')
