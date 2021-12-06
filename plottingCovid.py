3473e22570c3fc692b2790d747aaa22c3d6fc792
Funchhy <71510696+Funchhy@users.noreply.github.com>
2021-12-06 14:07:17 +0100

    Updated Data from RKI (06.12.2021)

--- plottingCovid.py
+++ plottingCovid.py
@@ -19,12 +19,17 @@ dfKoeln = dfKoeln.sort_values(by=['Refdatum'], ascending=False)
 
 dfKoeln.to_csv('csv_Data/koelnCovid.csv', encoding='utf-8')
 
-#Football Game - case reportings
+#Football Game - case reportings 2021/11/27
 dfKoeln = dfKoeln[['Refdatum', 'AnzahlFall']]
 dfKoeln = dfKoeln[dfKoeln['Refdatum'] >  datetime.datetime(2021,11,10)]
 
 dfKoeln = dfKoeln.set_index('Refdatum')
 
 dfKoeln.plot()
+markers = [datetime.datetime(2021,11,27), dfKoeln.loc[datetime.datetime(2021,11,27)]]
+plt.text(markers[0], markers[1],
+'Fussballspiel',horizontalalignment='right')
+plt.plot(markers[0], markers[1], 'o-')
+
 plt.show()
 
